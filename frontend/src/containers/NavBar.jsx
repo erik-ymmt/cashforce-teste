83605002c89f41d6d985c2329c76ff125c14e6bc
Erik Yamamoto <erik.ymmt@gmail.com>
2022-12-28 16:58:07 -0300

    feat: add favicon

--- frontend/src/containers/NavBar.jsx
+++ frontend/src/containers/NavBar.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import styles from './NavBar.module.css';
 
 import logo from '../assets/cashforceLogo.png'
-import { ReactComponent as Teste } from '../assets/favicon.svg'
-
+import handShake from '../assets/handShake.svg'
 
 function NavBar() {
   return (
     <div className={styles.container}>
       <img className={styles.logo} src={logo} alt="cashforce logo" />
       <nav>
-        <Teste />
+        <img src={handShake} alt="handshake" />
         Notas fiscais
       </nav>
     </div>
