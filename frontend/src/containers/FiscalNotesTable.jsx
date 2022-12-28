24916cf206612c1694cb71177ea4ced8b3fc2f47
Erik Yamamoto <erik.ymmt@gmail.com>
2022-12-28 17:29:37 -0300

    feat: add css

--- frontend/src/containers/FiscalNotesTable.jsx
+++ frontend/src/containers/FiscalNotesTable.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import styles from './FiscalNotesTable.module.css';
 
 function FiscalNotesTable() {
-  return <div>FiscalNotesTable</div>;
+  return (
+    <div className={styles.main_container}>
+      <header className={styles.header}></header>
+      <div className={styles.table_container}>
+        <h3> Notas Fiscais </h3>
+      </div>
+    </div>
+  );
 }
 
 export default FiscalNotesTable;
--- frontend/src/containers/NavBar.jsx
+++ frontend/src/containers/NavBar.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import styles from './NavBar.module.css';
 
-import logo from '../assets/cashforceLogo.png'
+import logo from '../assets/cashforceLogo.svg'
 import handShake from '../assets/handShake.svg'
 
 function NavBar() {
   return (
     <div className={styles.container}>
       <img className={styles.logo} src={logo} alt="cashforce logo" />
-      <nav>
+      <nav className={styles.navigation}>
         <img src={handShake} alt="handshake" />
-        Notas fiscais
+        <span>Notas fiscais</span>
       </nav>
     </div>
   );
