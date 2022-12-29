62e6bb2037ba63b141fc886cf52a674074ba4380
Erik Yamamoto <erik.ymmt@gmail.com>
2022-12-29 20:26:20 -0300

    feat: add integration test

--- backend/tests/order.test.js
+++ backend/tests/order.test.js
@@ -1,10 +1,21 @@
-const { expect } = require('chai');
-// const sinon = require('sinon');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+const app = require('../src/app');
+const services = require('../src/services');
+const { ordersMock } = require('./mocks/order.mocks');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
 
 describe('Order table endpoints tests', async () => {
-  describe('Find orders tests', async () => {
-    it('tests', async () => {
-      expect(1).to.be.equal(10);
-    });
+  let chaiHttpResponse;
+  afterEach(sinon.restore);
+  it('Find orders tests', async () => {
+    sinon.stub(services.orderService, 'findAll').resolves(ordersMock);
+    chaiHttpResponse = await chai.request(app).get('/orders');
+    expect(chaiHttpResponse.body).to.be.deep.equal(ordersMock);
+    expect(chaiHttpResponse).to.have.status(200);
   });
 });
