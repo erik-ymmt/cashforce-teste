530f36fae57f6acdeaae0d1ace0d03fa40866eb5
Erik Yamamoto <erik.ymmt@gmail.com>
2022-12-29 19:32:47 -0300

    chore: file configurations

--- backend/src/database/migrations/20221228135542-create-order.js
+++ backend/src/database/migrations/20221228135542-create-order.js
@@ -7,22 +7,22 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       orderNfId: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
-  async down(queryInterface, Sequelize) {
+  async down(queryInterface) {
     await queryInterface.dropTable('Orders');
-  }
+  },
 };
---
+++ backend/tests/order.test.js
@@ -0,0 +1,10 @@
+const { expect } = require('chai');
+// const sinon = require('sinon');
+
+describe('Order table endpoints tests', async () => {
+  describe('Find orders tests', async () => {
+    it('tests', async () => {
+      expect(1).to.be.equal(10);
+    });
+  });
+});
